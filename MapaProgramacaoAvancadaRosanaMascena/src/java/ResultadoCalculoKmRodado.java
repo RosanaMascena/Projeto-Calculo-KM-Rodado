09b1e407ccc16fbd42f680144943e451d72a4c5b
RosanaMascena <rosanamascena@gmail.com>
2022-10-26 20:41:25 -0300

    Arquivos gerais do projeto

---
+++ MapaProgramacaoAvancadaRosanaMascena/src/java/ResultadoCalculoKmRodado.java
@@ -0,0 +1,113 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+
+public class ResultadoCalculoKmRodado extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        String modeloCarro;
+        Double valorGasolina;
+        Double kmLitro;
+        Double valorRevisao;
+        Double valorPneus;
+        
+        Double calculoGasolinaKm;
+        Double calculoRevisao;
+        Double calculoPneus;
+        Double calculoTotal;
+        Double calculoAcrescimo;
+        
+        modeloCarro = request.getParameter("modelo-carro");
+        valorGasolina = Double.valueOf(request.getParameter("valor-gasolina"));
+        kmLitro = Double.valueOf(request.getParameter("km-litro"));
+        valorRevisao = Double.valueOf(request.getParameter("media-revisao"));
+        valorPneus = Double.valueOf(request.getParameter("valor-pneus"));
+        
+        calculoGasolinaKm = valorGasolina / kmLitro;
+        calculoRevisao = valorRevisao / 10000;
+        calculoPneus = valorPneus / 50000;
+        calculoTotal = calculoGasolinaKm + calculoRevisao + calculoPneus;
+        calculoAcrescimo = calculoTotal * 1.15;
+        
+        response.setContentType("text/html;charset=UTF-8");
+        try ( PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Resultado Cálculo Km Rodado</title>");
+            out.println("<link rel=\"stylesheet\" href=\"style.css\">");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<div class='content-resultado'><p>Cálculo Km Rodado para o Modelo <span class='span-resultado'>" + modeloCarro + "</span></p>");
+            out.println("<p class='p-resultado'>Valor da Gasolina - <span class='span-resultado'>" + valorGasolina + "</span></p>");
+            out.println("<p class='p-resultado'>Média do Veículo - <span class='span-resultado'>" + kmLitro + "</span></p>");
+            out.println("<p class='p-resultado'>Revisão 10 mil - <span class='span-resultado'>" + valorRevisao + "</span></p>");
+            out.println("<p class='p-resultado'>Pneus 50 mil - <span class='span-resultado'>" + valorPneus + "</span></p>");
+            out.println("<p class='p-resultado'>Valor do Km Rodado - <span class='span-resultado'>" + calculoTotal + "</span></p>");
+            out.println("<p class='p-resultado'>Com acréscimo de 15% - <span class='span-resultado'>" + calculoAcrescimo + "</span></p></div>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # Projeto-Calculo-KM-Rodado
- 
+
+Projeto realizado no curso Tecnólogo Sistemas para Internet, da disciplina de Programação Avançada.
+
+## Requesitos exigidos para o projeto:
+
+Criar um sistema WEB em Java que irá auxiliar os motoristas de aplicativo a calcular o custo do Km rodado.
+
+Para calcular o Km rodado, o usuário precisa inserir as seguintes informações:
+
+1 - Modelo do carro;
+
+2 – Valor do litro da gasolina;
+
+3 – Quantos km/l o carro faz;
+
+4 - Valor médio da revisão a cada 10 mil Km;
+
+5 - Valor médio de 4 pneus que rodam 50 mil Km.
+
+Para realizar o cálculo, você deve considerar e calcular o valor por km de gasolina gasto, o valor por km da revisão e o valor por km dos pneus. A soma desses custos chegará no valor do km rodado puro.
+
+Além disso estima-se que deve se acrescer de 15% no cálculo do Km rodado por desgaste e desvalorização do veículo.
+
+Ao final mostre uma página contendo todas as informações digitadas e o valor do Km rodado puro e o valor do Km rodado com os 15% de acréscimo.
+
+## Como uso a aplicação?
+
+Baixe e instale no seu computador o JDK(JAVA), em seguida a IDE NetBeans e também o Tomcat, que é um servidor Web Java. Logo após, instale o Tomcat no NetBeans.
+
+Baixe ou clone este repositório na sua máquina local, abra com a IDE NetBeans e execute.
+
+Prontinho, você estará visualizando o resultado do projeto.
+
+## Linguagens e pacotes utilizados:
+
+<div style="display: grid; grid-auto-flow:column; width: fit-content; gap: 0.5rem;">
+    <img height="30px" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original-wordmark.svg" />
+    <img height="30px" src="https://upload.wikimedia.org/wikipedia/commons/9/98/Apache_NetBeans_Logo.svg" />
+    <img height="30px" src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/fe/Apache_Tomcat_logo.svg/1280px-Apache_Tomcat_logo.svg.png" />
+</div>
